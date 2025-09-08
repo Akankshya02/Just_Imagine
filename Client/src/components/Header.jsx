442d22ab102cdc6ec0a8b5928ab65be1524dd453
Akankshya02 <akankshyapattnaik5@gmail.com>
2025-09-08 23:04:22 +0530

    header completed

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -10,10 +10,22 @@ const Header = () => {
         </p>
         <img src={assets.star_icon} alt="" />
       </div>
-      <h1 className='text-4xl max-w-[300px] sm:max-w-[590px] mx-auto mt-10 text-center'>Turn your imagination into <span className='text-pink-900'>image</span>,<br /> in seconds.</h1>
+      <h1 className='text-5xl max-w-[300px] sm:max-w-[590px] mx-auto mt-10 text-center'>Turn your imagination into <span className='text-pink-900'>Images</span>, in seconds.</h1>
 
-      <p></p>
+      <p className='text-sm max-w-[300px] sm:max-w-[590px] mx-auto mt-10 text-center'>Welcome to Just Imagine, where your words become art! Simply type a description of anything you can dream up—and our AI will bring it to life in a stunning, high-quality image. It's time to unleash your creativity and see what you can create.</p>
 
+      <button className='sm:text-lg text-white bg-pink-800 w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full '>
+        See the magic
+        <img className='h-6' src={assets.star_group} alt="" />
+      </button>
+
+      <div className='flex flex-wrap justify-center mt-8 gap-3'>
+        {Array(6).fill('').map((item, index)=>(
+            <img className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10 ' 
+            src={index % 2 == 0 ? assets.sample_img_1 : assets.sample_img_2} key={index} width={70}/>
+        ))}
+      </div>
+        <p className='mt-2 text-neutral-600'>Generated images from JustImagine</p>
     </div>
   )
 }
